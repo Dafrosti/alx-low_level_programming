45b81917a737d50c20221be595942496ae3acbd1
Egbule Obinna <kooljustrel@gmail.com>
2023-08-10 13:51:04 -0700

    a C program that prints using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Uses printf funtion to print "with proper grammer,
+ * but the outcome is a piece of art, followed by a new line
+ * Return: The Integer 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
---
+++ 0x00-hello_world/\
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Uses puts funtion to print "Programming is like
+ * building a multilingual puzzle
+ * Return: The Integer 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
